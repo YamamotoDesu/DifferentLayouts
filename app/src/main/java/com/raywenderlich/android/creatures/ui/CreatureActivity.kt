c21bef3e1a74694635aef73faddb678658d94710
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-23 13:01:08 +0900

    Introduction

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureActivity.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureActivity.kt
@@ -35,15 +35,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.model.Favorites
 import com.raywenderlich.android.creatures.model.Creature
 import com.raywenderlich.android.creatures.model.CreatureStore
 import kotlinx.android.synthetic.main.activity_creature.*
 
+
 class CreatureActivity : AppCompatActivity() {
 
   private lateinit var creature: Creature
+  private val adapter = FoodAdapter(mutableListOf())
 
   companion object {
     private const val EXTRA_CREATURE_ID = "EXTRA_CREATURE_ID"
@@ -63,6 +66,7 @@ class CreatureActivity : AppCompatActivity() {
     setupTitle()
     setupViews()
     setupFavoriteButton()
+    setupFoods()
   }
 
   private fun setupCreature() {
@@ -110,4 +114,12 @@ class CreatureActivity : AppCompatActivity() {
       }
     }
   }
+
+  private fun setupFoods(){
+    foodRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    foodRecyclerView.adapter = adapter
+    val foods = CreatureStore.getCreatureFoods(creature)
+    adapter.updateFoods(foods)
+  }
+
 }
